4faed05fc5c027742c9c0d551af213166a09ea35
Daniel-Chavarro <108764157+Daniel-Chavarro@users.noreply.github.com>
2023-04-15 12:09:55 -0500

    Added importation of data and input of user
    Added the read of the file .json to its ussage and storage of car plates to classify them, also added the input of the car plate for the user (JUST FOR CASE VIP), now i need to add the validation feature

--- Problem.py
+++ Problem.py
@@ -1,44 +1,66 @@
 import time
 import random as rand
-import os 
+import os
+import json
 
-diccionario = {}
+bahias = {}
+
+
+def clear():
+    os.system("cls")
+
+def validationPlate(data):
+    InpPlate = str(input("Ingese su Placa de Carro\n"))
+    InpPlate = InpPlate.upper()
+    
+
+def readData():
+    with open("car_Plates.json", encoding="utf-8") as f:
+        data = json.loads(f.read())
+    return print("Lectura de placas realizada correctamente"), data
+
 
 def Asig_Bahias():
     for i in range(1, 101):
         if i in range(1, 6):
-            diccionario[i] = "vip"
+            bahias[i] = "vip"
         elif i in range(61, 66):
-            diccionario[i] = "discapacitados"
+            bahias[i] = "discapacitados"
         elif i in range(80, 83):
-            diccionario[i] = "emergencia"
+            bahias[i] = "emergencia"
         elif i in range(98, 101):
-            diccionario[i] = "proovedores"
+            bahias[i] = "proovedores"
         else:
-            diccionario[i] = "normal"
-    
+            bahias[i] = "normal"
+
     return print("Asignación de Bahías realizada con exito")
 
-def Placeapark(carTipe):
+
+def Placeapark(carTipe, data):
     match carTipe:
         case 1:
-        
-        case 2:
-        
-        case 3:
-        
-        case 4:
-        
-        case 5:
-        
+            validationPlate(data)
+        # case 2:
+
+        # case 3:
+
+        # case 4:
+
+        # case 5:
 
 
 def run():
-    os.system("cls")
+    clear()
     Asig_Bahias()
+    data = readData()
     time.sleep(2)
-    carTipe = int(input("Selecciona el tipo de vehiculo: \n 1: VIP \n 2: Discapacitados \n 3: Emergencia \n 4: Proveedores \n 5: Normal \n"))
-    Placeapark(carTipe)
+    clear()
+    while True:
+        carTipe = int(input(
+            "Selecciona el tipo de vehiculo: \n 1: VIP \n 2: Discapacitados \n 3: Emergencia \n 4: Proveedores \n 5: Normal \n"))
+        clear()
+        Placeapark(carTipe, data)
+
 
 if __name__ == "__main__":
     run()
