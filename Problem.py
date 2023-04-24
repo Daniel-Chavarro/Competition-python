e044f890fabf5f48fef2e79cf205113e804f3d46
Daniel-Chavarro <108764157+Daniel-Chavarro@users.noreply.github.com>
2023-04-24 06:34:56 -0500

    a

--- Problem.py
+++ Problem.py
@@ -2,35 +2,104 @@ import time
 import random as rand
 import os
 import json
+import re
 
-bahias = []
 
+regex = r'^[a-zA-Z]{3}\d{3}$'
+
+# Variable listas para las bahias disponibles
+
+Bvip = []
+Bdisc = []
+Bemer = []
+Bprov = []
+Bcomm = []
+
+#Variables listas  para las zonas, usa las otras listas
+BzoneA = [i for i in (Bvip + Bcomm) if i in range(1,31)]
+BzoneB = [i for i in Bcomm if i in range(31,61)]
+BzoneC = [i for i in (Bcomm + Bemer + Bdisc + Bprov )if i in range(61,101)]
+
+#Limpia el cmd
 
 def clear():
     os.system("cls")
 
 
-def validationPlate():
-    InpPlate = str(input("Ingese su Placa de Carro\n"))
+def validation_AsignationPlate():
+    while True:
+        InpPlate = str(input(f"Ingese su Placa de Carro\n"))
+        if re.match(regex, InpPlate):
+            break
+        else:
+            print("La placa no es válida")
+            continue
+    
     InpPlate = InpPlate.upper()
     for i in data:
         if InpPlate == i:
             match data[i]:
                 case "vip":
-                    return InpPlate, data[i]
-                
+                    if len(Bvip) != 0:
+                        placeP = rand.choice(Bvip)
+                        Bvip.remove(placeP)
+                    else:
+                        placeP = min(Bcomm)
+                        Bcomm.remove(placeP)
+
+                    return InpPlate, data[i], placeP
                 case "discapacitados":
-                    return InpPlate, data[i]
+                    if len(Bdisc) != 0:
+                        placeP = rand.choice(Bdisc)
+                        Bdisc.remove(placeP)
+                        return InpPlate, data[i], placeP
+                    else:
+                        if len(Bdisc) != 0:
+                            placeP = rand.choice(Bdisc)
+                            Bdisc.remove(placeP)
+                        else:
+                            maxnumLastb = max([i for i in Bcomm if i in range(66, 80)])
+                            minumNextb = min([i for i in Bcomm if i in range(83,98)])
+                            if (80-maxnumLastb) < (minumNextb-82):
+                                placeP = maxnumLastb
+                                Bcomm.remove(placeP)
+                            else:
+                                placeP = minumNextb
+                                Bcomm.remove(placeP)
 
                 case "emergencia":
-                    return InpPlate, data[i]
+                    if len(Bemer) != 0:
+                        placeP = rand.choice(Bemer)
+                        Bemer.remove(placeP)
+                    else:
+                        maxnumLastb = max([i for i in Bcomm if i in range(6, 61)])
+                        minumNextb = min([i for i in Bcomm if i in range(66,80)])
+                        if (80-maxnumLastb) < (minumNextb-82):
+                            placeP = maxnumLastb
+                            Bcomm.remove(placeP)
+                        else:
+                            placeP = minumNextb
+                            Bcomm.remove(placeP)
+
+                    return InpPlate, data[i], placeP
 
                 case "proveedores":
-                    return InpPlate, data[i]
-                    
+                    if len(Bprov) != 0:
+                        placeP = rand.choice(Bprov)
+                        Bprov.remove(placeP)
+                    else:
+                        placeP = max(Bcomm)
+                        Bcomm.remove(placeP)
+
+                    return InpPlate, data[i], placeP
+
         else:
             continue
-    return InpPlate, "normal"
+
+    if len(Bcomm) != 0:
+        placeP = rand.choice(Bcomm)
+        Bcomm.remove(placeP)
+        return InpPlate, "normal", placeP
 
 
 def readData():
@@ -43,30 +112,47 @@ def readData():
 def Asig_Bahias():
     for i in range(1, 101):
         if i in range(1, 6):
-            bahias.append("vip")
+            Bvip.append(i)
         elif i in range(61, 66):
-            bahias.append("discapacitados")
+            Bdisc.append(i)
         elif i in range(80, 83):
-            bahias.append("emergencia")
+            Bemer.append(i)
         elif i in range(98, 101):
-            bahias.append("proveedores")
+            Bprov.append(i)
         else:
-            bahias.append("normal")
+            Bcomm.append(i)
 
     return print("Asignación de Bahías realizada con exito")
-
-
 
 
 def run():
     clear()
     readData()
-    Asig_Bahias()
+    Asig_Bahias() 
     time.sleep(2)
     clear()
     while True:
-        Cplate, type = validationPlate()
-        print(bahias.values())
+        register = int(input("0: Ingresar al parqueadero \n1: Salir del parqueadero \n Ingrese el numero \n"))
+        match register:
+            case 0:
+                BzoneA = [i for i in (Bvip + Bcomm) if i in range(1,31)]
+                BzoneB = [i for i in Bcomm if i in range(31,61)]
+                BzoneC = [i for i in (Bcomm + Bemer + Bdisc + Bprov )if i in range(61,101)]
+                print(f"""
+                        {len(Bvip)} Bahias vip dispoibles                                   
+                        {len(Bdisc)} Bahías discapacitados disponibles                      {len(BzoneA)} Bahias disponibles en zona A
+                        {len(Bemer)} Bahías emergecia disponibles                           {len(BzoneB)} Bahias disponibles en zona B
+                        {len(Bprov)} Bahias proveedores disponibles                         {len(BzoneC)} Bahias disponibles en zona C
+                        {len(Bcomm)} Bahias comunes disponibles
+                        
+                        {len(BzoneA+BzoneB+BzoneC)} Bahias en Total
+                        """)                       
+                
+                Cplate, type, place = validation_AsignationPlate()
+                clear()
+                print(f"{Cplate}\nBahía {type}\nBahía número {place}")
+            case 1:
+                
 
 
 if __name__ == "__main__":
