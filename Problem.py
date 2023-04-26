00e44e22018e58e7d2bc0fdf437262b0d0ac47d0
Daniel-Chavarro <108764157+Daniel-Chavarro@users.noreply.github.com>
2023-04-25 22:20:16 -0500

    Almost last commit
    review of the park completed
    Something to be carefull:
       solve some bugs in the code
       the sound of advertisment of almost and no space left in the park

--- Problem.py
+++ Problem.py
@@ -4,6 +4,29 @@ import os
 import json
 import re
 
+# Contador para el tiempo total según zonas
+
+TimeA = 0
+TimeB = 0
+TimeC = 0
+
+# Contador para el dinero recolectado por las tarifas
+
+Counter_money = 0
+
+# Contadores para el numero de vehiculos que ingresaron según el tipo de bahía
+
+Counter_Vip = 0
+Counter_Disc = 0
+Counter_Emer = 0
+Counter_Prov = 0
+Counter_Comm = 0
+
+# Contadores para el numero de vehiculos que ingresaron según la zona
+
+Counter_zoneA = 0
+Counter_zoneB = 0
+Counter_zoneC = 0
 
 regex = r'^[a-zA-Z]{3}\d{3}$'
 
@@ -16,6 +39,7 @@ Bdisc = []
 Bemer = []
 Bprov = []
 Bcomm = []
+
 
 # Variables listas  para las zonas, usa las otras listas
 BzoneA = [i for i in (Bvip + Bcomm) if i in range(1, 31)]
@@ -30,6 +54,8 @@ def clear():
 
 
 def validation_AsignationPlate():
+    global Counter_Comm, Counter_Disc, Counter_Emer, Counter_Prov, Counter_Vip
+
     while True:
         InpPlate = str(input(
             f"Ingese su Placa de Carro. En caso de haber dado por error, digitar 0:\n"))
@@ -53,19 +79,20 @@ def validation_AsignationPlate():
                         placeP = min(Bcomm)
                         Bcomm.remove(placeP)
 
+                    Counter_Vip += 1
                     return InpPlate, data[i], placeP
                 case "discapacitados":
                     if len(Bdisc) != 0:
                         placeP = rand.choice(Bdisc)
                         Bdisc.remove(placeP)
-                        return InpPlate, data[i], placeP
+
                     else:
                         if len(Bdisc) != 0:
                             placeP = rand.choice(Bdisc)
                             Bdisc.remove(placeP)
                         else:
                             maxnumLastb = max(
-                                [i for i in Bcomm if i in range(66, 80)])
+                                [i for i in Bcomm if i in range(1, 80)])
                             minumNextb = min(
                                 [i for i in Bcomm if i in range(83, 98)])
                             if (80-maxnumLastb) < (minumNextb-82):
@@ -75,15 +102,18 @@ def validation_AsignationPlate():
                                 placeP = minumNextb
                                 Bcomm.remove(placeP)
 
+                    Counter_Disc += 1
+                    return InpPlate, data[i], placeP
+
                 case "emergencia":
                     if len(Bemer) != 0:
                         placeP = rand.choice(Bemer)
                         Bemer.remove(placeP)
                     else:
                         maxnumLastb = max(
-                            [i for i in Bcomm if i in range(6, 61)])
+                            [i for i in Bcomm if i in range(1, 61)])
                         minumNextb = min(
-                            [i for i in Bcomm if i in range(66, 80)])
+                            [i for i in Bcomm if i in range(66, 98)])
                         if (80-maxnumLastb) < (minumNextb-82):
                             placeP = maxnumLastb
                             Bcomm.remove(placeP)
@@ -91,6 +121,7 @@ def validation_AsignationPlate():
                             placeP = minumNextb
                             Bcomm.remove(placeP)
 
+                    Counter_Emer += 1
                     return InpPlate, data[i], placeP
 
                 case "proveedores":
@@ -101,6 +132,7 @@ def validation_AsignationPlate():
                         placeP = max(Bcomm)
                         Bcomm.remove(placeP)
 
+                    Counter_Prov += 1
                     return InpPlate, data[i], placeP
 
         else:
@@ -109,6 +141,7 @@ def validation_AsignationPlate():
     if len(Bcomm) != 0:
         placeP = rand.choice(Bcomm)
         Bcomm.remove(placeP)
+        Counter_Comm += 1
         return InpPlate, "normal", placeP
 
 
@@ -136,6 +169,7 @@ def Asig_Bahias():
 
 
 def run():
+    global Counter_zoneA, Counter_zoneB, Counter_zoneC, Counter_money, TimeA, TimeB, TimeC
     clear()
     readData()
     Asig_Bahias()
@@ -143,11 +177,12 @@ def run():
     clear()
     while True:
         register = str(input(
-            "0: Ingresar al parqueadero \n1: Salir del parqueadero \n Ingrese el numero \n"))
+            "0: Ingresar al parqueadero \n1: Salir del parqueadero \n2: Finalizar parqueadero y entregar informe \nIngrese el numero \n"))
         if register == "0":
             BzoneA = [i for i in (Bvip + Bcomm) if i in range(1, 31)]
             BzoneB = [i for i in Bcomm if i in range(31, 61)]
-            BzoneC = [i for i in (Bcomm + Bemer + Bdisc + Bprov)if i in range(61, 101)]
+            BzoneC = [i for i in (Bcomm + Bemer + Bdisc +
+                                  Bprov)if i in range(61, 101)]
             print(f"""
                     {len(Bvip)} Bahias vip dispoibles                                   
                     {len(Bdisc)} Bahías discapacitados disponibles                      {len(BzoneA)} Bahias disponibles en zona A
@@ -163,10 +198,21 @@ def run():
                 clear()
                 continue
             else:
+                if place in BzoneA:
+                    zone = "A"
+                    Counter_zoneA += 1
+                elif place in BzoneB:
+                    zone = "B"
+                    Counter_zoneB += 1
+                elif place in BzoneC:
+                    zone = "C"
+                    Counter_zoneC += 1
                 clear()
-                print(f"{Cplate}\nBahía {type}\nBahía número {place}")
+                print(f"{Cplate}\nBahía {type}\nZona {zone} \nBahía número {place}")
 
-            Bused[Cplate] = {"num_bahia": place, "type_bahia": type}
+            Bused[Cplate] = {"num_bahia": place,
+                             "type_bahia": type, "time_arrived": time.time()}
+
         elif register == "1":
             clear()
             while True:
@@ -193,15 +239,77 @@ def run():
                     case "proveedores":
                         Bprov.append(Bused[Cplate]["num_bahia"])
                         Bprov.sort()
+
+                        time_arrived = Bused[Cplate]["time_arrived"]
+                        time_exit = time.time()
+                        time_total = time_exit - time_arrived
+                        minutes_total = time_total / 60
+                        if round(int(minutes_total)) > 30:
+                            print(
+                                f"Gracias por usar el Parqueadero, el tiempo de uso fue {round(int(minutes_total))} Minutos, Debe pagar una tarifa de $5000 Pesos")
+                            Counter_money += 5000
+                        else:
+                            print(
+                                f"Gracias por usar el Parqueadero, el tiempo de uso fue {round(int(minutes_total))} Horas")
+
                     case "normal":
                         Bcomm.append(Bused[Cplate]["num_bahia"])
                         Bcomm.sort()
+
+                time_arrived = Bused[Cplate]["time_arrived"]
+                time_exit = time.time()
+                time_total = time_exit - time_arrived
+                hours_total = time_total / 3600
+
+                if Bused[Cplate]["num_bahia"] in range(1, 31):
+                    TimeA += hours_total
+                elif Bused[Cplate]["num_bahia"] in range(31, 61):
+                    TimeB += hours_total
+                elif Bused[Cplate]["num_bahia"] in range(61, 101):
+                    TimeC += hours_total
+
                 del Bused[Cplate]
+
                 clear()
-                print("Gracias por usar el Parqueadero.")
+                print(
+                    f"Gracias por usar el Parqueadero, el tiempo de uso fue {round(hours_total, 2)} Horas")
             else:
                 clear()
-                print("La placa ingresada no se encuentra registrada.")
+                print(f"La placa ingresada no se encuentra registrada.")
+
+        elif register == "2":
+            password = str(input("Ingrese la contraseña:"))
+            if password == "^@3fXn%V#92oJAD2":
+                clear()
+                print(f"""
+                Resultados Día de hoy:
+                Total ingresados por tipo de Bahía:
+                {Counter_Comm} Commun
+                {Counter_Disc} Discapacitados
+                {Counter_Emer} Emergencia
+                {Counter_Prov} Proveedores
+                {Counter_Vip} Vip
+                
+                Total Ingresos por Zona:
+                {Counter_zoneA} Zona A
+                {Counter_zoneB} Zona B
+                {Counter_zoneC} Zona C
+
+                Dinero Recolectado por tarifa de poveedores: ${Counter_money} Pesos
+                ${Counter_money/2} Pesos para Mejoramiento de parqueadero
+                ${Counter_money/2} Pesos para Promocion de Estudiantes
+
+                Tiempo segun la zona:
+                {TimeA} Horas en la zona A
+                {TimeB} Horas en la zona B
+                {TimeC} Horas en la zona C
+                """)
+
+                time.sleep(60)
+                break
+            else:
+                clear()
+                print("Contraseña invalida")
 
         else:
             clear()
